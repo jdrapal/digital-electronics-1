659bcda169ae0dfd4238ce3a6d871846bd916c81
student <student@PC-636.urel.feec.vutbr.cz>
2022-04-14 14:54:28 +0200

    projekt

---
+++ labs/project/project_morse/project_morse.srcs/sources_1/new/morse_reciever.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/14/2022 01:16:32 PM
+-- Design Name: 
+-- Module Name: morse_reciever - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity morse_reciever is
+    Port ( clk : in STD_LOGIC;
+           input : in STD_LOGIC;
+           rst : in STD_LOGIC);
+end morse_reciever;
+
+architecture Behavioral of morse_reciever is
+
+begin
+
+
+end Behavioral;
